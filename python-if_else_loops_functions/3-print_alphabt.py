fb80f23c23a002224d0dc9570e6496c691a24c02
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-27 17:58:42 +0100

    qe

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z') + 1):
-	if alphabet != ord('e') and alphabet != ord('q'):
-		print("{}".format(chr(alphabet)), end='')
+for alphabet in range(ord('a'), ord('z')+1):
+    if (alphabet != 101) and (alphabet != 113):
+        print("{}".format(chr(alphabet)), end="")
