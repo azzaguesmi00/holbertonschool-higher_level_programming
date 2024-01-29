e3abc34cf4762c18a7d312b5f576c9c9a5dee489
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-29 22:07:27 +0100

    lasdig

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if abs(number) > 9:
-        lasdig = abs(number) % 10
-    elif 0 <= abs(number) <= 9:
-        lasdig = abs(number)
-    print(lasdig, end="")
-    return lasdig            
+    last = abs(number) % 10
+    print("{}".format(last), end="")
+    return last
