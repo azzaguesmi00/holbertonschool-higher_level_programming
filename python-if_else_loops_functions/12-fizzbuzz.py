d666c6f03bc89d56b03d5ae30bf1859c65bbb39d
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-29 22:20:41 +0100

    fuzzbuzzo

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for numbr in range(1, 101):
-        if numbr % 5 == 0 and numbr % 3 != 0:
-            print("Buzz ", end="")
-        elif numbr % 3 == 0 and numbr % 5 != 0:
-            print("Fizz ", end="")
-        elif numbr % 5 == 0 and numbr % 3 == 0:
-            print("FizzBuzz ", end="")
-        else:
-            print(f"{numbr}", end=" ")
-            
+	for numbr in range(1, 101):
+		if numbr % 5 == 0 and numbr % 3 != 0:
+			print("Buzz ", end="")
+		elif numbr % 3 == 0 and numbr % 5 != 0:
+			print("Fizz ", end="")
+		elif numbr % 5 == 0 and numbr % 3 == 0:
+			print("FizzBuzz ", end="")
+		else:
+			print(f"{numbr}", end=" ")
+			
