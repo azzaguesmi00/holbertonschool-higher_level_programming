7f977943c0f7cf4a484e0dcc52cf9e1161ef8c24
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-29 22:11:05 +0100

    lasdigohh

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,3 @@ def print_last_digit(number):
     last = abs(number) % 10
     print("{}".format(last), end="")
     return last
-# Test cases
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
