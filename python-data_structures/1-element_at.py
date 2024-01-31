d85c07e1b7309c038985231b456fe5a0afc65c27
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-31 16:07:31 +0100

    printing element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0 and idx < len(my_list):
+    if idx < 0 or not my_list:
+        return
+    if idx < len(my_list):
         return my_list[idx]
-    else:
-        return None
+    return
