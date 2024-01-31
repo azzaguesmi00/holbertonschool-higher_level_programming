c619114e2fb95ec2376e42603b1b90268e130fec
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-31 16:21:25 +0100

    cursed list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    curs_list = my_list[::-1]
-    for elf in curs_list:
-        print("{:d}".format(elf))
-        
+	curs_list = my_list[::-1]
+	for elf in curs_list:
+		print("{:d}".format(elf))
+		
