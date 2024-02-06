d06523a44c0ef27fa6f9b75221ed81202612da85
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-06 12:36:28 +0100

    hazem

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-	if idx < 0 or not my_list:
-		return
-	if idx < len(my_list):
-		return my_list[idx]
-	return
+    if idx < 0 or not my_list:
+        return
+    if idx < len(my_list):
+        return my_list[idx]
+    return
