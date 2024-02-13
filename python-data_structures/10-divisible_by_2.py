f77a57bad20209e78c838264e24e2919cf5133d7
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-13 19:10:13 +0100

    div by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result_list = []
+    for num in my_list:
+        multiple_2 = num % 2 == 0
+        result_list.append(multiple_2)
+
+        return result_list
