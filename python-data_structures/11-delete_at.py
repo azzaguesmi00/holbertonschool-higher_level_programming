bc18c0d1b067adc5bd7d19cd55b95c58af33778d
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-15 12:33:09 +0100

    delete2

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,6 +3,8 @@ def delete_at(my_list=[], idx=0):
     
     if 0 <= idx < len(my_list):
         result_list = my_list[:idx] + my_list[idx + 1:]
-        return result_list
-    else:
-        return my_list
+        my_list.remove()
+        my_list.extend(result_list)
+    return my_list
+    
+   
