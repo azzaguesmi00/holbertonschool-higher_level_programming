20e5268e0d56a0b8870ee93cb46a568c417c8e59
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-15 14:59:30 +0100

    2d array

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result_matrix = [[0 for _ in range(len(row))] for row in matrix]
-    for i in range(len(matrix)):
-        for j in range (len(matrix[i])):
-            result_matrix[i][j] == matrix[i][j] ** 2
+    result_matrix = []
+    for i in matrix:
+        result_matrix.append(list(map(lambda x: x**2,i)))
             
     return result_matrix
         
