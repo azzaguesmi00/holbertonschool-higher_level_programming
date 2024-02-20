08721ca9af6a3d0e7532b69f61a949ef0241580c
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-20 23:57:44 +0100

    scrpt1

---
+++ python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of the Square class.
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+
+if __name__ == "__main__":
+    pass
