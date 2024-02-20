618cbd387164e56988a324b03bb9e4dc86c744e2
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-20 23:12:53 +0100

    sript2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    integers_printed = 0
-
-    try:
-        for i in range(x):
-            value = my_list[i]
-            print("{:d}".format(value), end=' ')
-            integers_printed += 1
-
-    except (ValueError, TypeError, IndexError):
-        pass
-
-    finally:
-        print()
-
-    return integers_printed
+     element = 0
+for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            element += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+print()
+return element
