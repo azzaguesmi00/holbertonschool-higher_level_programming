9d51ecd94aefe89d55b45074d169baf7565f5b36
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-20 23:52:26 +0100

    script0

---
+++ python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of the Square class.
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+    
+    This class is currently empty and will be extended to include
+    attributes and methods for handling square-related operations.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
