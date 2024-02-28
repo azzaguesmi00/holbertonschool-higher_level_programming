2a630e9a3ac4588793c2542e215df04663fe9ec7
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-02-28 21:55:28 +0100

    script1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Rectangle class with private width and height attribute"""
+    
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle instance.
+        
+        Args:
+            width: The width of the rectangle. Default is 0.
+            height: The height of the rectangle. Default is 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter method for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
