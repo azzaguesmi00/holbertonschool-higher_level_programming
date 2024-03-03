d53f1b90d6a4859fe943a67ccbbfdadf90c660a3
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-03-03 18:40:55 +0100

    script2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defining append-write function"""
+
+def append_write(filename="", text=""):
+    """append filename with utf-8"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
