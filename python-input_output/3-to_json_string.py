df9939edc24550d719d12ff8d3d1f8ba34599146
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-03-03 18:47:13 +0100

    script3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defining to_json_string function"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    return json representation of an object
+        my_obj: object
+    """
+    return json.dumps(my_obj)
