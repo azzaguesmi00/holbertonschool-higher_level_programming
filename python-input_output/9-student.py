c29392102af0aec6c78130b7596ac4c12215ebad
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-03-03 19:42:36 +0100

    script9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""containing the class "student" """
+"""
+    file input-output
+"""
 
-class student:
+
+class Student:
     """
-    Representation of a student
-    that defines a student
+    a function that defines a student
     public instance attributes:
     first_name -- str
     last_name -- str
     age -- int
     """
-    
-    
-    def __init__(self , first_name, last_name, age):
 
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
-        """returns a dictionnary representation of a student instance"""
+        """retrieve a dictionary representation of student"""
         return self.__dict__
