b5b0eba6fbba640764b193db9deb58782814adac
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-03-03 19:41:38 +0100

    script9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -13,7 +13,7 @@ class student:
     
     
     def __init__(self , first_name, last_name, age):
-        """Initializes the student"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
