13afb68205c67252118cda4d78d42abb710a9841
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-03-03 17:08:49 +0100

    scriot8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,11 +6,19 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """A class to represent a rectangle."""
 
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    def __str__(self):
-        """Return a string representation of the Rectangle."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        self.__width = height
+
+
+# def area(self):
+#    raise Exception("area() is not implemented")
+
+# def integer_validator(self, name, value):
+#    if type(value) is not int:
+#        raise TypeError(f"{name} must be an integer")
+#    if value < 1:
+#        raise ValueError(f"{name} must be greater than 0")
