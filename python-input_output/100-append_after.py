e3f4f4793af00d44da9f23451750bdca211e90d1
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-03-03 20:14:34 +0100

    script100

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -2,8 +2,11 @@
 """containing the append_after function"""
 
 def append_after(filename="", search_string="", new_string=""):
+    
     """appends new-string after a line containing
     search_string in filename """
+    
+    
     with open (filename, 'r', encoding='utf-8') as f:
         link_list = []
         while True:
